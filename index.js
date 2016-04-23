f6d20b2973a2ac1adea21cc2d1cd3f5d5043b0e0
ChalkPE <chalkpe@gmail.com>
2016-04-23 19:51:17 +0000

    Implemented basic structure

--- index.js
+++ index.js
@@ -1,23 +1,108 @@
 var chalk = require('chalk');
-
+var moment = require('moment');
 var keypress = require('keypress');
-keypress(process.stdin);
 
+keypress(process.stdin);
 process.stdin.setRawMode(true);
 process.stdin.resume();
 
 process.stdin.on('keypress', function(ch, key){
-    if(key && key.ctrl && key.name === 'c') process.stdin.pause();
-
+    if(key && key.ctrl && key.name === 'c'){
+        process.stdin.pause();
+        process.exit(1);
+    }
 });
 
-var fields = Array.apply(null, Array(process.stdout.columns)).map(() => Array.apply(null, Array(process.stdout.rows)));
-var clear = () => process.stdout.write('\033c');
+const FieldType = {
+    GROUND: 0,
+    BLANK: 1,
+    MINE: 2
+};
 
-for(var y = 0; y < process.stdout.rows; y++){
-    for(var x = 0; x < process.stdout.columns; x++){
-        process.stdout.write((x + y) % 2 ? '_' : '-');
-    }
+const Border = {
+    HORIZONTAL: '━',
+    VERTICAL: '┃',
+    TOP_LEFT: '┏',
+    TOP_RIGHT: '┓',
+    BOTTOM_LEFT: '┗',
+    BOTTOM_RIGHT: '┛',
+    COLOR: chalk.yellow
 }
 
-setTimeout(clear, 5000);
+var consoleWidth = process.stdout.columns;
+var consoleHeight = process.stdout.rows;
+
+var fieldWidth = consoleWidth - 4;
+var fieldHeight = consoleHeight - 3;
+
+var fields = Array.apply(null, Array(fieldWidth)).map(() => Array.apply(null, Array(fieldHeight)).map(() => FieldType.GROUND));
+
+var newLine = () => process.stdout.write('\n');
+var write = (text, ln) => {
+    process.stdout.write(text);
+    if(ln) newLine();
+};
+
+var clear = () => write('\033c');
+var repeat = (theChalk, text, count) => {
+    for(var i = 0; i < count; i++) write(theChalk(text));
+};
+
+const Title = {
+    COLOR: chalk.yellow.bold
+};
+
+var printTitle = (text, align) => {
+    var spaces = consoleWidth - text.length;
+
+    switch(align){
+        case 'left':
+            write(Title.COLOR(text));
+            repeat(Title.COLOR, ' ', spaces);
+            break;
+
+        default:
+        case 'center':
+            var leftSpaces = Math.floor(spaces / 2);
+            repeat(Title.COLOR, ' ', leftSpaces);
+            write(Title.COLOR(text));
+            repeat(Title.COLOR, ' ', spaces - leftSpaces);
+            break;
+
+        case 'right':
+            repeat(Title.COLOR, ' ', spaces);
+            write(Title.COLOR(text));
+            break;
+    }
+
+    process.stdout.write('\n');
+};
+
+var random = (min, max) => {
+    if(max === undefined){
+        max = min; min = 0;
+    }
+
+    return min + Math.floor(Math.random() * (max - min));
+};
+
+var print = () => {
+    clear();
+    printTitle(moment().toString());
+
+    write(Border.COLOR(Border.TOP_LEFT));
+    repeat(Border.COLOR, Border.HORIZONTAL, consoleWidth - 2);
+    write(Border.COLOR(Border.TOP_RIGHT));
+
+    for(var y = 0; y < fieldHeight; y++){
+        write(Border.COLOR(Border.VERTICAL + ' '));
+        for(var x = 0; x < fieldWidth; x++) write('' + random(10));
+        write(Border.COLOR(' ' + Border.VERTICAL));
+    }
+
+    write(Border.COLOR(Border.BOTTOM_LEFT));
+    repeat(Border.COLOR, Border.HORIZONTAL, consoleWidth - 2);
+    write(Border.COLOR(Border.BOTTOM_RIGHT));
+};
+
+setInterval(print, 500);
